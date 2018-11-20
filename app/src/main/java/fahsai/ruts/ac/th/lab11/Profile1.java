d7f7be3c8375716597e800b21020823f51dcfe20
a10305-5 <123>
2018-11-20 11:23:05 +0700

    Initial commit

---
+++ app/src/main/java/fahsai/ruts/ac/th/lab11/Profile1.java
@@ -0,0 +1,13 @@
+package fahsai.ruts.ac.th.lab11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Profile1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile1);
+    }
+}
